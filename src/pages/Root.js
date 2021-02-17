7af723b464caaed2fb1918904a59b576f0393795
mateuszral <mateuszral@onet.pl>
2021-02-17 22:09:47 +0100

    Add home page and file with routes

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { routes } from 'routes';
+
+import MainTemplate from 'templates/MainTemplate';
+
+import Home from 'pages/Home';
 
 const Root = () => {
-  return <div>Hello React</div>;
+  return (
+    <BrowserRouter>
+      <MainTemplate>
+        <Switch>
+          <Route exact path={routes.home} component={Home} />
+        </Switch>
+      </MainTemplate>
+    </BrowserRouter>
+  );
 };
 
 export default Root;
